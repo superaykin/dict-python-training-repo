50000a638f9d2a1833cf83ff83f3684fec77dd06
superaykin <aykin.lgupanabo@gmail.com>
2025-10-20 11:20:04 +0800

    structured

---
+++ simulations/SimulationDay01_ILP.py
@@ -0,0 +1,21 @@
+# intro
+print("Hello Python3!")
+print("Another print without semi-colon")
+print("Just another print line")
+
+# set variable
+varrr = 'var-value'
+print("Print with variable with value: " + varrr)
+
+# list
+thisIsList = ['1', '2', '3']
+print ( thisIsList )
+
+# function
+def myInfo() :
+    print ("Lastname: PALMA")
+    print ("Firstname: IKENN LOUIE")
+    print ("Middlename: M")
+
+myInfo()
+
